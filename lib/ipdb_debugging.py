c8291aa84eec62258d4ddd86cb1d9f1f7153d5b3
Michael Akoko <akokomichael37@gmail.com>
2024-09-30 22:58:44 +0300

    add script files

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
+# lib/ipdb_debugging.py
 import ipdb
 
 def plus_two(num):
-    num + 2
+    num = num + 2
     return num
+
+# lib/testing/ipdb_debugging_test.py
+import pytest
+from ipdb_debugging import plus_two
+
+class TestIpdbDebugging:
+    def test_adds_two(self):
+        assert(plus_two(3) == 5)
